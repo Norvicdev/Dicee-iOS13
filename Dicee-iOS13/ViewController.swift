867a0e4ef785c7eda31d12a01a68193cec1aab26
Norvicdev <norvicdev@gmail.com>
2022-07-22 22:35:17 +0300

    upd

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,12 +12,22 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageViewOne: UIImageView!
     
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
+        // здесь мы делаем замену, Кого.Что = Значение
+        //diceImageViewOne.alpha = 0.5
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
     }
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceFive")
+    }
+    
 
 }
 
