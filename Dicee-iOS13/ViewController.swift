9fefab7df6426ddd242a4a725cb22479a2007bc2
Norvicdev <norvicdev@gmail.com>
2022-07-25 15:15:19 +0300

    finished

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -4,28 +4,29 @@
 //
 //  Created by Angela Yu on 11/06/2019.
 //  Copyright © 2019 London App Brewery. All rights reserved.
-//
+// Vic Sergeev iOS bootcamp training 2022 25 july
 
 import UIKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageViewOne: UIImageView!
-    
-    
     @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
-        // здесь мы делаем замену, Кого.Что = Значение
-        //diceImageViewOne.alpha = 0.5
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
+//        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
+//        // здесь мы делаем замену, Кого.Что = Значение
+//        //diceImageViewOne.alpha = 0.5
+//        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
     }
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceFive")
+        //diceImageViewTwo.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageViewTwo.image = diceArray.randomElement() //первый вариантт рандома
+        diceImageViewOne.image = diceArray[Int.random(in:0...5)] // второй вариант рандома
     }
     
 
